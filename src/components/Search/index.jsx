ea172858df8875bc87b6b10a91ce8d9188711f49
BarryDong <286084845@qq.com>
2022-12-27 17:17:26 +0800

    给Search组件的button绑定click事件，获取input的值

--- src/components/Search/index.jsx
+++ src/components/Search/index.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react'
 
 class Search extends Component {
+  searchUser = () => {
+    console.log(this.inputElement.value)
+  }
   render() {
     return (
       <section className="jumbotron">
         <h3 className="jumbotron-heading">Search Github Users</h3>
         <div>
-          <input type="text" placeholder="enter the name you search"/>
-          <button>Search</button>
+          <input ref={node => this.inputElement = node} type="text" placeholder="enter the name you search"/>
+          <button onClick={this.searchUser}>Search</button>
         </div>
       </section>
     )
